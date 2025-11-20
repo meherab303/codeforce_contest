e6aac9631dd938453416be76c042b98d71acb1da
meherab nahin <nahinrahman87@gmail.com>
2025-11-20 21:52:06 +0600

    div 3 A

---
+++ A_Shizuku_Hoshikawa_and_Farm_Legs.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  
+  int t;cin>>t;
+  while(t--){
+    int x;cin>>x;
+    if(x%2!=0){
+        cout<<0<<endl;
+    }else{
+        int y=x/2;
+        cout<<(y/2)+1<<endl;
+    }
+  } 
+  return 0;
+}
